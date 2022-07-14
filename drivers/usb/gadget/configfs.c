39bbff023a68390485d437296ed500ce955991fa
Anan Jaser <ananjaser@gmail.com>
2022-07-15 02:58:25 +0400

    drivers: usb: gadget: adjust bcdUSB descriptor
    * USB Super-speed is set to 0x0320 on N975F And N770F So lets try it
    * also use set_usb_enumeration_state(cdev->desc.bcdUSB);
    * and beautify from N975F

--- drivers/usb/gadget/configfs.c
+++ drivers/usb/gadget/configfs.c
@@ -905,7 +905,7 @@ static inline struct gadget_info *os_desc_item_to_gadget_info(
 
 static ssize_t os_desc_use_show(struct config_item *item, char *page)
 {
-	return sprintf(page, "%d",
+	return sprintf(page, "%d\n",
 			os_desc_item_to_gadget_info(item)->use_os_desc);
 }
 
@@ -929,7 +929,7 @@ static ssize_t os_desc_use_store(struct config_item *item, const char *page,
 
 static ssize_t os_desc_b_vendor_code_show(struct config_item *item, char *page)
 {
-	return sprintf(page, "%d",
+	return sprintf(page, "0x%02x\n",
 			os_desc_item_to_gadget_info(item)->b_vendor_code);
 }
 
@@ -1068,7 +1068,7 @@ static inline struct usb_os_desc_ext_prop
 
 static ssize_t ext_prop_type_show(struct config_item *item, char *page)
 {
-	return sprintf(page, "%d", to_usb_os_desc_ext_prop(item)->type);
+	return sprintf(page, "%d\n", to_usb_os_desc_ext_prop(item)->type);
 }
 
 static ssize_t ext_prop_type_store(struct config_item *item,
@@ -1595,12 +1595,7 @@ static void android_work(struct work_struct *data)
 		pr_info("usb: %s sent uevent %s\n", __func__, connected[0]);
 		uevent_sent = true;
 #ifdef CONFIG_USB_TYPEC_MANAGER_NOTIFIER
-		if (gi->connected) {
-			if (cdev->desc.bcdUSB == 0x310)
-				set_usb_enumeration_state(0x310);	// Super-Speed
-			else
-				set_usb_enumeration_state(0x210);	// High-Speed
-		}
+		set_usb_enumeration_state(cdev->desc.bcdUSB);
 #endif
 	}
 
